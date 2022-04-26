5371d2dc6d65d005b01ddf918b6508f2faadc75c
Felipendev <felipe.psic@outlook.com.br>
2022-04-26 12:23:48 -0300

    Exclui arquivos criados erroneamente e add corretos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ErrosComponent } from './erros/erros.component';
-import { SharedComponent } from './shared/shared.component';
-import { ViewsComponent } from './views/views.component';
 import { EnvioDeMensagemComponent } from './views/envio-de-mensagem/envio-de-mensagem.component';
-import { NotFoundComponent } from './erros/not-found/not-found.component';
+import { NotFoundComponent } from './erros/erros/not-found/not-found.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { ErrosModule } from './erros/erros/erros.module';
+import {AccordionModule} from 'primeng/accordion'; 
+import {ToastModule} from 'primeng/toast';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrosComponent,
-    SharedComponent,
-    ViewsComponent,
     EnvioDeMensagemComponent,
     NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ErrosModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ConfirmDialogModule,
+    AccordionModule,
+    ToastModule,
+    BrowserAnimationsModule
+
+    
+    
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/envio-de-mensagem/envio-de-mensagem.component.ts
+++ src/app/views/envio-de-mensagem/envio-de-mensagem.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Email } from 'src/app/shared/envia-mensagem/envia-mensagem.model';
 
+import { DialogService } from 'primeng/dynamicdialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { EmailService } from 'src/app/shared/email.service';
 @Component({
   selector: 'app-envio-de-mensagem',
   templateUrl: './envio-de-mensagem.component.html',
-  styleUrls: ['./envio-de-mensagem.component.css']
+  styleUrls: ['./envio-de-mensagem.component.css'],
+  providers: [DialogService, ConfirmationService, MessageService]
 })
 export class EnvioDeMensagemComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  emails!: Observable<Email[]>;
+  position!: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    public dialogService: DialogService,
+    private confirmationService: ConfirmationService,
+    private service: EmailService,
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
+
+    this.form = this.fb.group({
+      ownerRef: ["Desenvolvedor JAVA"],
+      emailFrom: ["dev.especialista.java@gmail.com"],
+      emailTo: ["fhps.dev@gmail.com"],
+      subject: ["PODCAST Pode Questionar"],
+      text: [null, [this.validarObrigatoriedade]]
+    });
+  }
+
+  enviaEmail() {
+    this.service.sendEmail(this.form.value).subscribe(success => {
+      console.log("Pergunta enviada com sucesso!")
+      this.messageService.add({ severity: 'success', summary: 'Tudo certo!', detail: 'Pergunta enviada com sucesso!' });
+      setTimeout(function () {
+        window.location.reload();
+      }, 1000);
+    },
+      error => {
+        console.log("Falha ao enviar pergunta!")
+        this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'Error ao enviar pergunta, tente novamente!' }),
+      () => console.log('Pergunta enviada com sucesso!')});
+  }
+
+
+  validarObrigatoriedade(input: FormControl) {
+    return (input.value ? null : { obrigatoriedade: true });
   }
 
 }
